f08277e9f5dae8dce126039838207311f9e5917a
jochuu <reyn@live.co.uk>
2024-07-04 23:53:49 +1000

    setup: add cards, add resources, update styling
    cards now exist for all subsections, added images for trending section, major styling updates (colours, animations)

---
+++ js/main.js
@@ -0,0 +1,38 @@
+let resultContainer = document.querySelector(".resultContainer");
+function determineSplitType(textbox) {
+  if (/[,]/.test(textbox)) {
+    return ",";
+  }
+  if (/[ ]/.test(textbox)) {
+    return " ";
+  }
+  if (/[\t]/.test(textbox)) {
+    return "\t";
+  }
+}
+document.querySelector("#addressBtn").onclick = () => {
+  let latLongTextBox = $("#latlong").val();
+  let splitType = determineSplitType(latLongTextBox);
+  resultContainer.textContent = "";
+
+  let latLongArr = latLongTextBox.split("\n");
+  latLongArr.forEach((latlong) => {
+    let latlongArr = latlong.split(splitType);
+    let latitude = latlongArr[0].trim();
+    let longitude = latlongArr[1].trim();
+    $.getJSON(
+      "https://nominatim.openstreetmap.org/reverse",
+      {
+        lat: latitude,
+        lon: longitude,
+        format: "json",
+      },
+      function (result) {
+        console.log(result);
+        let address = document.createElement("div");
+        address.textContent = `[${latitude}${splitType} ${longitude}] ${result.display_name}`;
+        resultContainer.appendChild(address);
+      }
+    );
+  });
+};
